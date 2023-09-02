d8ce491222caa765e8b130f08468ece77727fbdd
Rob Van Mieghem <robvanmieghem@gmail.com>
2023-09-02 14:28:24 +0200

    Add issuer to assetpage

--- lib/pages/assetpage.dart
+++ lib/pages/assetpage.dart
@@ -13,38 +13,43 @@ class AssetPage extends StatelessWidget {
         appBar: createSimpleAppBar(context, context.read<Asset>().code),
         body: Center(
             child: Consumer<Asset>(
-                builder: (context, asset, child) => Column(children: [
-                      Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: ListTile(
-                              title: Text(asset.info.name ?? asset.code),
-                              subtitle: Text(asset.info.domain ?? ''),
-                              leading: asset.isNative()
-                                  ? const Icon(PlanetIcon.xlmIcon)
-                                  : asset.info.image != null
-                                      ? Image(
-                                          image:
-                                              NetworkImage(asset.info.image!))
-                                      : null,
-                              trailing: Text('${asset.amount}'))),
-                      if (asset.info.description != null)
-                        Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Text(asset.info.description!)),
-                      Row(children: [
-                        FilledButton.tonal(
-                            onPressed: () {},
-                            child: const Row(children: [
-                              Text('Send'),
-                              Icon(Icons.arrow_upward)
-                            ])),
-                        FilledButton.tonal(
-                            onPressed: () {},
-                            child: const Row(children: [
-                              Text('Swap'),
-                              Icon(Icons.swap_horiz)
-                            ])),
-                      ]),
-                    ]))));
+                builder: (context, asset, child) => Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: ListTile(
+                                  title: Text(asset.info.name ?? asset.code),
+                                  subtitle: Text(asset.info.domain ?? ''),
+                                  leading: asset.isNative()
+                                      ? const Icon(PlanetIcon.xlmIcon)
+                                      : asset.info.image != null
+                                          ? Image(
+                                              image: NetworkImage(
+                                                  asset.info.image!))
+                                          : null,
+                                  trailing: Text('${asset.amount}'))),
+                          Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Text('Issuer: ${asset.issuer}')),
+                          if (asset.info.description != null)
+                            Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(asset.info.description!)),
+                          Row(children: [
+                            FilledButton.tonal(
+                                onPressed: () {},
+                                child: const Row(children: [
+                                  Text('Send'),
+                                  Icon(Icons.arrow_upward)
+                                ])),
+                            FilledButton.tonal(
+                                onPressed: () {},
+                                child: const Row(children: [
+                                  Text('Swap'),
+                                  Icon(Icons.swap_horiz)
+                                ])),
+                          ]),
+                        ]))));
   }
 }
